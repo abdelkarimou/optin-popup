ccb31e51844cadd7245044693f47b140cc7410b6
Abdelkarim Oufkir <150941042+abdelkarimou@users.noreply.github.com>
2024-01-15 11:15:41 +0100

    fix

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-    <link rel="stylesheet" href="assets/custom.css">
+    <link rel="stylesheet" href="/assets/custom.css">
     <title>Subscription Form</title>
 </head>
 
@@ -126,7 +126,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="assets/main.js"></script>
+    <script src="/assets/main.js"></script>
 </body>
 
 </html>
